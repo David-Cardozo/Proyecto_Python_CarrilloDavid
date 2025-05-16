a23170e2924269f1bdbf23c86e7ade4f1432d443
CarrilloDavid <MiniMrSanty@gmail.com>
2025-05-16 12:38:23 -0500

    Feat: :sparkles: the project starts

---
+++ main.py
@@ -0,0 +1,16 @@
+from dataProyect.datos import *
+
+from dataProyect.logs import *
+
+dataBool = True
+
+while (dataBool):
+    print("=========================================================================")
+    print("                      Simulador de Gasto Diario                          ")
+    print("=========================================================================")
+    print("Seleccione una opcion:   ")
+    print("1. Registrar nuevo gasto")
+    print("2. Listar gastos")
+    print()
+    print()
+    print()
